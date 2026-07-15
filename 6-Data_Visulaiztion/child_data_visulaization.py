37fd1a954ef772e1b2687ba9bbe176286b08d5bf
Muhammad Ajwad Rana <ranaajwad498@example.com>
2026-07-15 13:18:16 +0500

    Data Visulaization

---
+++ 6-Data_Visulaiztion/child_data_visulaization.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import pandas as pd 
+
+class children:
+    def __init__(self,df):
+        self.df= df
+
+    def display_columns(self):
+        self.df.colums
+
+    def create_gender_chart(self):
+        count =self.df["Gender"].value_counts()
+        print(count)
+        plt.bar(count.index, count.values)
+        plt.grid(True)
+        plt.xlabel("Gender")
+        plt.ylabel("Number of Children")
+        plt.savefig("charts/gender_chart.png")
+        plt.show()
+
+
+    def create_risk_category_chart(self):
+        counts = self.df["Nutrition_Status"].value_counts()
+        print(counts)
+        plt.pie(counts, labels=counts.index, autopct="%1.0f%%")
+        plt.title("Nutrition_Status")
+        plt.savefig("charts/risk_category_distribution.png")
+        plt.show()
+
+    def create_age_distribution_chart(self):
+        plt.hist(self.df["Age (months)"])
+        plt.grid(True)
+        plt.title("Chart of Child's Age Distibutation")
+        plt.xlabel("Child Age (Months)")
+        plt.ylabel("Number of Children")
+        plt.savefig("charts/age_distribution.png")
+        plt.show()
+    
+    def create_all_charts(self):
+        print("             ===== Child Nutrition Data Visualization =====")
+        print("Dataset loaded successfully.")
+        print("Creating Chart 1: Children by Gender...")
+        self.create_gender_chart()
+        print("Creating Chart 2: Risk Category Distribution...")
+        self.create_risk_category_chart()
+        print("Creating Chart 3: Age Distribution...")
+        self.create_age_distribution_chart()
+
+        
+        
+
+
+df = pd.read_csv("malnutrition_children_ethiopia.csv")
+child = children(df)
+child.create_all_charts()
