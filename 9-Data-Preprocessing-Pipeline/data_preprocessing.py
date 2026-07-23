6229856d7af98b323a4ae8dbeec3f638f84beb79
Muhammad Ajwad Rana <ranaajwad498@gmail.com>
2026-07-23 15:49:45 +0500

    Data Preprocessing

---
+++ 9-Data-Preprocessing-Pipeline/data_preprocessing.py
@@ -0,0 +1,78 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler 
+from sklearn.model_selection import train_test_split
+class ChildDataPreprocessor:
+    def __init__(self, file_path="child_malnutrition_features.csv"):
+        self.file_path=file_path
+        
+
+    def load_data(self):
+        self.df = pd.read_csv(self.file_path)
+        print("Dataset Shape:", self.df.shape)
+        print("First 5 Rows:")
+        print(self.df.head())
+        print("Columns:\n",self.df.columns.tolist())
+        if not self.df.empty:
+            print(f"Success! Loaded {len(self.df)} rows and {len(self.df.columns)} columns.")
+        else:
+            print("Warning! The loaded DataFrame is empty.")
+        return self.df
+
+    def inspect_data(self):
+        print("-"*50)
+        print(f"Total Number of Rows:{len(self.df)} \n Total number of Columns: {len(self.df.columns)}")
+        print("-"*50)
+        print("Data Type of Each Column \n",self.df.dtypes)
+        print("-"*50)
+        print("Missing Values in Each Columns:\n", self.df.isnull().sum())
+        print("-"*50)
+        print("Duplicate Records:\n", self.df.duplicated().sum())
+        print("-"*50)
+        self.numeric_columns = self.df.select_dtypes(include='int64').columns.tolist()
+        print("Numeric column names:",self.numeric_columns)
+        self.categorical_columns = self.df.select_dtypes(include="str").columns.tolist()
+        print("Categorical columns names:",self.categorical_columns)
+
+    def clean_data(self):
+        self.df=self.df.drop_duplicates()
+        self.df= self.df.drop(columns=['ID' ,'Anemia', 'Malaria', 'Diarrhea', 'TB'])
+        numeric_columns=self.df.select_dtypes(include="int64")
+        for col in numeric_columns:
+            self.df[col] = self.df[col].fillna(self.df[col].mean(),inplace=True)
+        categorical_columns= self.df.select_dtypes(include="str")
+        for col in categorical_columns:
+            self.df[col] = self.df[col].fillna(self.df[col].mode()[0],inplace=True)
+            return self.df.shape
+
+    def select_target(self):
+        self.target = self.df["Nutrition_Status"]
+        print(f"Our target Column is: {self.target}")
+
+    def split_features_target(self):
+        X = self.df.drop(columns=[self.target])
+        Y = self.df[self.target]
+
+    def encode_categorical_features(self):
+        self.X = pd.get_dummies(self.X)
+        if self.Y.dtype == "str":
+            self.label_encoder = LabelEncoder()
+            self.Y = self.label_encoder.fit_transform(self.Y)
+
+    def scale_numerical_features(self):
+        numeric_columns=self.df.select_dtypes(include="int64")
+        minmax= MinMaxScaler()
+        self.X = minmax.fit_transform(self.X[numeric_columns])
+
+    # def split_training_testing_data(self):
+        # X_train, X_test, Y_train, Y_test = (self.X, self.Y, test_size=0.2, random_state= 42, stratify=y)
+
+    def display_report(self):
+        print("Orignal Shape of our DataSet:",self.df.shape)
+        print("Cleaned DataSet Shape:", self.clean_data())
+
+
+child = ChildDataPreprocessor()
+df = child.load_data()
+child.display_report()
+
+
