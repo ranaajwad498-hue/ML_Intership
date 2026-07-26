bcfe52fd04fc31288261112483775f4db11e159d
Muhammad Ajwad Rana <ranaajwad498@gmail.com>
2026-07-26 22:45:44 +0500

    Trained Model

---
+++ 10-Model_Training/model_training.py
@@ -0,0 +1,66 @@
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+import joblib
+
+class ChildRiskLogisticRegression:
+    def __init__(self, x_train= "x_train.csv", x_test="x_test.csv", y_train="y_train.csv", y_test="y_test.csv"):
+        self.x_train=x_train
+        self.x_test= x_test
+        self.y_train=y_train
+        self.y_test=y_test
+
+    def load_data(self):
+        self.x_train=pd.read_csv(self.x_train)
+        self.x_test=pd.read_csv(self.x_test)
+        self.y_train=pd.read_csv(self.y_train)
+        self.y_test=pd.read_csv(self.y_test)
+
+    def create_model(self):
+        self.base_model = LogisticRegression(max_iter=1000)
+        self.model = MultiOutputClassifier(self.base_model)
+        return self.model
+
+    def train_model(self):
+        self.model.fit(self.x_train, self.y_train)
+        print("Model is Trained Successfully")
+
+    def make_predictions(self):
+        self.prediction=self.model.predict(self.x_test)
+        return self.prediction
+
+    def predict_proba(self):
+        print("Probability:",self.model.predict_proba(self.x_test))
+
+    def save_model(self):
+        joblib.dump(self.model, "Trained Model.pkl")
+        print("Model is Saved Successfully")
+
+    def load_model(self):
+        self.load = joblib.load("Trained Mode.pkl")
+        return self.load
+
+    def predict_single_child():
+        print("Predicting New Child")
+        new_child= pd.DataFrame([{
+            
+
+        }])
+
+
+    # def evaluate_model(self):
+    #     # print("Accuracy:",accuracy_score(self.y_test, self.prediction))
+    #     print("Cofusion Matrix:",confusion_matrix(self.y_test, self.prediction))
+    #     print("Classfication Report:",classification_report(self.y_test, self.prediction))
+
+
+pipline = ChildRiskLogisticRegression("x_train.csv", "x_test.csv", "y_train.csv", "y_test.csv")
+pipline.load_data()
+pipline.create_model()
+pipline.train_model()
+pipline.make_predictions()
+pipline.predict_proba()
+pipline.evaluate_model()
+
+
---
+++ 10-Model_Training/split.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+
+raw_data= pd.read_csv("malnutrition_children_ethiopia.csv")
+cleaned_data = raw_data.drop(columns=["ID"])
+
+le= LabelEncoder()
+string_data = cleaned_data.select_dtypes(include="object")
+for col in string_data:
+    cleaned_data[col]=le.fit_transform(cleaned_data[col])
+
+
+x= cleaned_data.drop(columns=['Stunting',
+       'Underweight', 'Overweight', 'Anemia', 'Malaria', 'Diarrhea', 'TB',
+       'Nutrition_Status'])
+y = cleaned_data[['Stunting',
+       'Underweight', 'Overweight', 'Anemia', 'Malaria', 'Diarrhea', 'TB',
+       'Nutrition_Status']]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+# cleaned_data.to_csv("Cleaned Data.csv" ,index=False)
+# x_test.to_csv("x_test.csv",index= False)
+# x_train.to_csv("x_train.csv",index= False)
+# y_test.to_csv("y_test.csv",index= False)
+# y_train.to_csv("y_train.csv",index= False)
+
+# print(cleaned_data.columns)
+print("X_train columns:", pd.read_csv('x_train.csv').columns.tolist())
+print("y_train columns:", pd.read_csv('y_train.csv').columns.tolist())
