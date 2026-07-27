d71a9f1ee2a31d0c05339d080f588c976aeba29e
Muhammad Ajwad Rana <ranaajwad498@gmail.com>
2026-07-27 15:02:54 +0500

    Update Code

--- 10-Model_Training/model_training.py
+++ 10-Model_Training/model_training.py
@@ -6,16 +6,21 @@ import joblib
 
 class ChildRiskLogisticRegression:
     def __init__(self, x_train= "x_train.csv", x_test="x_test.csv", y_train="y_train.csv", y_test="y_test.csv"):
-        self.x_train=x_train
-        self.x_test= x_test
-        self.y_train=y_train
-        self.y_test=y_test
+        self.x_train_file=x_train
+        self.x_test_file= x_test
+        self.y_train_file=y_train
+        self.y_test_file=y_test
+        self.x_train=None
+        self.x_test = None
+        self.y_train= None
+        self.y_test= None
+        self.prediction= None
 
     def load_data(self):
-        self.x_train=pd.read_csv(self.x_train)
-        self.x_test=pd.read_csv(self.x_test)
-        self.y_train=pd.read_csv(self.y_train)
-        self.y_test=pd.read_csv(self.y_test)
+        self.x_train=pd.read_csv(self.x_train_file)
+        self.x_test=pd.read_csv(self.x_test_file)
+        self.y_train=pd.read_csv(self.y_train_file)
+        self.y_test=pd.read_csv(self.y_test_file)
 
     def create_model(self):
         self.base_model = LogisticRegression(max_iter=1000)
@@ -43,18 +48,20 @@ class ChildRiskLogisticRegression:
         self.load = joblib.load("Trained Mode.pkl")
         return self.load
 
-    def predict_single_child():
+    def predict_single_child(self):
         print("Predicting New Child")
         new_child= pd.DataFrame([{
-            
-
+           "Age (months)":31,"Gender":0,"Region":2,"Mother_Education":3,"Household_Wealth_Index":0,"Height_cm":90,"Weight_kg":15
         }])
-
+        self.prediction= self.model.predict(new_child)
+        print("New Child",self.prediction)
+        return self.prediction
 
     # def evaluate_model(self):
-    #     # print("Accuracy:",accuracy_score(self.y_test, self.prediction))
-    #     print("Cofusion Matrix:",confusion_matrix(self.y_test, self.prediction))
-    #     print("Classfication Report:",classification_report(self.y_test, self.prediction))
+    #     for col in ['Stunting','Underweight', 'Overweight', 'Anemia', 'Malaria', 'Diarrhea', 'TB','Nutrition_Status']:
+    #         accuracy = self.y_test[col]
+    #         print(f"Accuracy Score of {col} is:",accuracy_score(accuracy, self.prediction))
+            
 
 
 pipline = ChildRiskLogisticRegression("x_train.csv", "x_test.csv", "y_train.csv", "y_test.csv")
@@ -63,4 +70,5 @@ pipline.create_model()
 pipline.train_model()
 pipline.make_predictions()
 pipline.predict_proba()
-pipline.evaluate_model()
+# pipline.evaluate_model()
+pipline.predict_single_child()
--- 10-Model_Training/split.py
+++ 10-Model_Training/split.py
@@ -20,12 +20,12 @@ y = cleaned_data[['Stunting',
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-# cleaned_data.to_csv("Cleaned Data.csv" ,index=False)
-# x_test.to_csv("x_test.csv",index= False)
-# x_train.to_csv("x_train.csv",index= False)
-# y_test.to_csv("y_test.csv",index= False)
-# y_train.to_csv("y_train.csv",index= False)
+cleaned_data.to_csv("Cleaned Data.csv" ,index=False)
+x_test.to_csv("x_test.csv",index= False)
+x_train.to_csv("x_train.csv",index= False)
+y_test.to_csv("y_test.csv",index= False)
+y_train.to_csv("y_train.csv",index= False)
 
 # print(cleaned_data.columns)
-print("X_train columns:", pd.read_csv('x_train.csv').columns.tolist())
-print("y_train columns:", pd.read_csv('y_train.csv').columns.tolist())
+# print("X_train columns:", pd.read_csv('x_train.csv').columns.tolist())
+# print("y_train columns:", pd.read_csv('y_train.csv').columns.tolist())
