95c6fb5c682b6f115e6d1c7e3fbf03be696ba7e5
Ajwad Rana <ranaajwad498@gmail.com>
2026-09-25 03:01:29 +0500

    Testing Backend and Creatinf Frontend

---
+++ 26-React-Forms-API/21-JWT-Authentication/database.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+load_dotenv()
+
+database_url = os.getenv("DATABASE_URL")
+engine = create_engine(database_url)
+session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+base = declarative_base()
+
